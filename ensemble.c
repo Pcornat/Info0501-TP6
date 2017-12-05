c40759ed06df103d81f309280dc4205da056f77f
postaron <florent.denef@gmail.com>
2017-12-05 20:07:05 +0100

    début implémentation des ensembles

---
+++ ensemble.c
@@ -0,0 +1,13 @@
+#include "ensemble.h"
+
+void creerEnsemble(sommet_t *x) {
+
+}
+
+void trouverEnsemble(sommet_t *x) {
+
+}
+
+void Union(sommet_t *x, sommet_t *y) {
+
+}
---
+++ ensemble.h
@@ -0,0 +1,10 @@
+#ifndef ENSEMBLE_H_
+#define ENSEMBLE_H_
+
+#include "algo_parcours.h"
+
+void creerEnsemble(sommet_t *x);
+void trouverEnsemble(sommet_t *x);
+void Union(sommet_t *x, sommet_t *y);
+
+#endif /* ENSEMBLE_H_ */
