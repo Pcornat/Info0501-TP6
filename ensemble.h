62332d159f3f88b3dcfe5bb369c513d6c18328d9
postaron <florent.denef@gmail.com>
2017-12-24 16:00:21 +0100

    Pas de c++ GitHub

--- ensemble.h
+++ ensemble.h
@@ -5,6 +5,6 @@
 
 void creerEnsemble(sommet_t *x);
 void trouverEnsemble(sommet_t *x);
-void Union(sommet_t *x, sommet_t *y);
+void union_ensemble(sommet_t *x, sommet_t *y);
 
 #endif /* ENSEMBLE_H_ */
