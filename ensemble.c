e9e60cbaa961c0ce587582bf8bc55a86180d59e0
postaron <florent.denef@gmail.com>
2017-12-24 16:04:59 +0100

    oubli de commit celui-ci avec le dernier commit

--- ensemble.c
+++ ensemble.c
@@ -1,13 +1,16 @@
 #include "ensemble.h"
 
-void creerEnsemble(sommet_t *x) {
+void creerEnsemble(sommet_t *x)
+{
 
 }
 
-void trouverEnsemble(sommet_t *x) {
+void trouverEnsemble(sommet_t *x)
+{
 
 }
 
-void Union(sommet_t *x, sommet_t *y) {
+void union_ensemble(sommet_t *x, sommet_t *y)
+{
 
 }
