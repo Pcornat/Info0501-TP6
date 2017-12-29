1a750410053f14470ab15284d46156502798fb08
postaron <florent.denef@gmail.com>
2017-12-29 15:38:45 +0100

    Ajout de la structure de donnée correspondant à un ensemble (approximativement)

--- ensemble.h
+++ ensemble.h
@@ -2,9 +2,16 @@
 #define ENSEMBLE_H_
 
 #include "algo_parcours.h"
+#include "cellule.h"
 
-void creerEnsemble(sommet_t *x);
-void trouverEnsemble(sommet_t *x);
-void union_ensemble(sommet_t *x, sommet_t *y);
+typedef struct
+{
+	cellule_t *tete;
+	cellule_t *queue;
+} set_t;
+
+void creerEnsemble(set_t *a, sommet_t *x);
+void trouverEnsemble(set_t *a, sommet_t *x);
+void union_ensemble(set_t *a, sommet_t *x, sommet_t *y);
 
 #endif /* ENSEMBLE_H_ */
