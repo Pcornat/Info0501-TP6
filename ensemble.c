f12af4971bfee73b4e09c52c8cd3f71cda43e33d
postaron <florent.denef@gmail.com>
2018-01-05 21:24:34 +0100

    Structure de données des ensembles disjoints faite !

--- ensemble.c
+++ ensemble.c
@@ -1,16 +1,61 @@
 #include "ensemble.h"
 
-void creerEnsemble(sommet_t *x)
-{
+#include <stdlib.h>
 
+set_t* creerEnsembleVide(void)
+{
+	set_t *a = (set_t*) malloc(sizeof(set_t));
+	a->queue = a->tete = NULL;
+	a->size = 0;
+	return a;
 }
 
-void trouverEnsemble(sommet_t *x)
+set_t* creerEnsemble(cell_ensemble_t *sommet)
 {
-
+	set_t *a = (set_t*) malloc(sizeof(set_t));
+	a->tete = a->queue = NULL;
+	a->queue = a->tete = sommet;
+	a->tete->representant = a;
+	a->size = 1;
+	return a;
 }
 
-void union_ensemble(sommet_t *x, sommet_t *y)
+set_t* trouverEnsemble(cell_ensemble_t *x)
 {
+	return x->representant;
+}
 
+/**
+ * Application avec l'heuristique de l'union pondérée
+ * pour un temps d'exécution plus faible.
+ */
+set_t* union_ensemble(cell_ensemble_t *x, cell_ensemble_t *y)
+{
+	set_t *c = NULL, *a = x->representant, *b = y->representant;
+	cell_ensemble_t *cell = NULL;
+	c = creerEnsembleVide();
+	c->size = a->size + b->size;
+	if (a->size >= b->size)
+	{
+		c->tete = a->tete;
+		c->queue = b->queue;
+		for (cell = b->tete; cell != NULL; cell = cell->succ)
+		{
+			cell->representant = c;
+		}
+		a->queue->succ = b->tete;
+	}
+	else
+	{
+		c->tete = b->tete;
+		c->tete = a->queue;
+		for (cell = a->tete; cell != NULL; cell = cell->succ)
+		{
+			cell->representant = c;
+		}
+		b->queue->succ = a->tete;
+	}
+	free(a), free(b);
+	a = b = NULL;
+	return c;
 }
--- ensemble.h
+++ ensemble.h
@@ -1,17 +1,18 @@
 #ifndef ENSEMBLE_H_
 #define ENSEMBLE_H_
 
-#include "algo_parcours.h"
-#include "cellule.h"
+#include "cellEnsemble.h"
 
-typedef struct
+typedef struct setter_t
 {
-	cellule_t *tete;
-	cellule_t *queue;
+	cell_ensemble_t *tete;
+	cell_ensemble_t *queue;
+	int size; /* Heuristique de l'union pondérée */
 } set_t;
 
-void creerEnsemble(set_t *a, sommet_t *x);
-void trouverEnsemble(set_t *a, sommet_t *x);
-void union_ensemble(set_t *a, sommet_t *x, sommet_t *y);
+set_t* creerEnsembleVide(void);
+set_t* creerEnsemble(cell_ensemble_t *sommet);
+set_t* trouverEnsemble(cell_ensemble_t *x);
+set_t* union_ensemble(set_t **a, set_t **b); /* Plus rapide d'ajouter un ensemble petit à la queue d'un ensemble grand. */
 
 #endif /* ENSEMBLE_H_ */
