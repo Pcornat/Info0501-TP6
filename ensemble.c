1290e260a3ed40e9a5147d7d1a6106c9820435bb
postaron <florent.denef@gmail.com>
2018-01-08 20:34:11 +0100

    Correction de certaines fonctions.

--- ensemble.c
+++ ensemble.c
@@ -29,33 +29,28 @@ set_t* trouverEnsemble(cell_ensemble_t *x)
  * Application avec l'heuristique de l'union pondérée
  * pour un temps d'exécution plus faible.
  */
-set_t* union_ensemble(cell_ensemble_t *x, cell_ensemble_t *y)
+void union_ensemble(cell_ensemble_t *x, cell_ensemble_t *y)
 {
-	set_t *c = NULL, *a = x->representant, *b = y->representant;
+	set_t *a = x->representant, *b = y->representant;
 	cell_ensemble_t *cell = NULL;
-	c = creerEnsembleVide();
-	c->size = a->size + b->size;
-	if (a->size >= b->size)
+	if (b->size <= a->size)
 	{
-		c->tete = a->tete;
-		c->queue = b->queue;
+		a->size += b->size;
+		a->queue->succ = b->tete;
+		a->queue = b->queue;
 		for (cell = b->tete; cell != NULL; cell = cell->succ)
 		{
-			cell->representant = c;
+			cell->representant = a;
 		}
-		a->queue->succ = b->tete;
 	}
 	else
 	{
-		c->tete = b->tete;
-		c->tete = a->queue;
+		b->size += a->size;
+		b->queue->succ = a->tete;
+		b->queue = a->queue;
 		for (cell = a->tete; cell != NULL; cell = cell->succ)
 		{
-			cell->representant = c;
+			cell->representant = b;
 		}
-		b->queue->succ = a->tete;
 	}
-	free(a), free(b);
-	a = b = NULL;
-	return c;
 }
--- ensemble.h
+++ ensemble.h
@@ -13,6 +13,6 @@ typedef struct setter_t
 set_t* creerEnsembleVide(void);
 set_t* creerEnsemble(cell_ensemble_t *sommet);
 set_t* trouverEnsemble(cell_ensemble_t *x);
-set_t* union_ensemble(cell_ensemble_t *x, cell_ensemble_t *y); /* Plus rapide d'ajouter un ensemble petit à la queue d'un ensemble grand. */
+void union_ensemble(cell_ensemble_t *x, cell_ensemble_t *y); /* Plus rapide d'ajouter un ensemble petit à la queue d'un ensemble grand. */
 
 #endif /* ENSEMBLE_H_ */
